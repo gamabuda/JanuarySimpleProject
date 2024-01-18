98f4d80c73e1b7cf82fa4e1ecbdab9b8c625e230
Илиза Галина <ilizagalina4@gmail.com>
2024-01-18 21:17:43 +0300

    add the ability to delete a list of objects

--- JanuarySimpleProject/Core/Node.cs
+++ JanuarySimpleProject/Core/Node.cs
@@ -133,15 +133,36 @@ namespace JanuarySimpleProject.Core
             string strValue = value.ToString().Trim();
 
             if (strValue == null)
-                return;
+                throw new Exception("Значение = нул");
 
             if (!_values.Contains(strValue))
-                return;
+                throw new Exception("Значение уже есть");
 
             _values.Remove(strValue);
             _value = Value.Replace(strValue, "");
 
             OnNodeChange?.Invoke();
+        }
+        public void RemoveValue<TValue>(List<TValue> values)
+        {
+            if (values.Count <= 0)
+                throw new Exception("Пустой массив");
+
+            foreach (var value in values)
+            {
+                string strValue = value.ToString().Trim();
+
+                if (strValue == null)
+                    throw new Exception("Значение = нул");
+
+                if (_values.Contains(strValue))
+                    throw new Exception("Значение уже есть");
+
+                _values.Remove(strValue);
+                _value += $"{strValue}";
+
+                OnNodeChange?.Invoke();
+            }
         }
         public static Node CreateEmptyNode()
         {
