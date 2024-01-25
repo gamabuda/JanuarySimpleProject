65ac3eb56b9dce4c9d788bd35af49439ef6797cb
Asus <Asus@DESKTOP-F2RLVQ4>
2024-01-25 22:25:33 +0300

    динамик аррэй

--- JanuarySimpleProject/Core/DynamicArray.cs
+++ JanuarySimpleProject/Core/DynamicArray.cs
@@ -4,37 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace JanuarySimpleProject.Core
+namespace JanuarySimpleProject
 {
-    internal class DynamicArray
+    internal class DArray<T> where T : IComparable<T>
     {
-        private string[] _array;
+        private T[] _array;
         private int _count;
 
-        public DynamicArray()
+        public DArray(int len)
         {
-            _array = new string[4];
+            _array = new T[len];
             _count = 0;
         }
 
-        public void Add(string str)
+        private void ArrSize()
+        {
+            int _count1 = _count + _count / 2;
+            T[] item = new T[_count1];
+            if (_count == 0)
+            {
+                _count1 = 20;
+            }
+            for (int i = 0; i < _array.Length; i++)
+            {
+                item[i] = _array[i];
+            }
+            _array = item;
+        }
+
+        public void Add(T s)
         {
             if (_count == _array.Length)
             {
-                //TODO написать свой
-                Array.Resize(ref _array, _array.Length * 2);
+                ArrSize();
             }
-            _array[_count] = str;
+            _array[_count] = s;
             _count++;
         }
 
-        public void Remove(string str)
+        public void Print()
         {
-            int index = Array.IndexOf(_array, str, 0, _count);
-
-            if(index != -1)
+            foreach (var s in _array)
             {
-                for(int i = 0; i < _count - 1; i++)
+                if (s != null)
+                    Console.WriteLine(s);
+            }
+        }
+
+        public void Remove(T s)
+        {
+            int index = -1;
+            for (int i = 0; i < _count; i++)
+            {
+                if (_array[i].Equals(s))
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            if (index != -1)
+            {
+                for (int i = index; i < _count - 1; i++)
                 {
                     _array[i] = _array[i + 1];
                 }
@@ -42,23 +73,83 @@ namespace JanuarySimpleProject.Core
             }
         }
 
-        public bool Contains(string str)
+        public bool Contains(T s)
         {
-            return Array.IndexOf(_array, str, 0, _count) != -1;
-        }
-
-        public void Print()
-        {
-            for (int i = 0; i < _count; i++)
+            if (_array.Contains(s))
             {
-                Console.Write(_array[i] + " ");
+                return true;
             }
-            Console.WriteLine();
+            else
+            {
+                return false;
+            }
         }
 
-        public int Count
+        public void Clear()
         {
-            get { return _count; }
+            var Arr = new T[15];
+            _array = Arr;
+            _count = 0;
+        }
+
+        public T[] GetArray()
+        {
+            return _array;
+        }
+
+        public int Count()
+        {
+            return _count;
+        }
+
+        public int BinarySearch(T s)
+        {
+            int left = 0;
+            int right = _count - 1;
+            while (left <= right)
+            {
+                int mid = (left + right) / 2;
+                if (_array[mid].CompareTo(s) == 0)
+                {
+                    return mid;
+                }
+                else if (_array[mid].CompareTo(s) < 0)
+                {
+                    left = mid + 1;
+                }
+                else
+                {
+                    right = mid - 1;
+                }
+            }
+            return -1;
+        }
+
+        public void Sort()
+        {
+            for (int i = 0; i < _count - 1; i++)
+            {
+                for (int j = i + 1; j < _count; j++)
+                {
+                    if (_array[i].CompareTo(_array[j]) > 0)
+                    {
+                        T temp = _array[i];
+                        _array[i] = _array[j];
+                        _array[j] = temp;
+                    }
+                }
+            }
+        }
+
+        internal void Insert(int index, string item)
+        {
+            throw new NotImplementedException();
+        }
+
+        internal void RemoveAt(int index)
+        {
+            throw new NotImplementedException();
         }
     }
+
 }
