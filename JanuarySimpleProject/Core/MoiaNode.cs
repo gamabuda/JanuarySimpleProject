542feef5235e7644ff872e73205d22464511b57e
polinabydugina <xch.victor@mail.ru>
2024-01-27 01:07:53 +0300

    моия нода есть да

---
+++ JanuarySimpleProject/Core/MoiaNode.cs
@@ -0,0 +1,38 @@
+﻿using JanuarySimpleProject.Core.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuarySimpleProject.Core
+{
+    public class MoiaNode : INode
+    {
+        public MoiaNode()
+        {
+
+        }
+        string Id { get; }
+        string Name { get; set; }
+        string Value { get; set; }
+        DateTime DateTimeCreate { get; }
+        DateTime DateTimeUpdate { get; }
+
+        void ShowInfo()
+        {
+
+        }
+        void AddValue<TValue>(TValue value)
+        {
+
+        }
+        void RemoveValue<TValue>(TValue value)
+        {
+
+        }
+      
+
+        event Action OnNodeChange;
+    }
+}
