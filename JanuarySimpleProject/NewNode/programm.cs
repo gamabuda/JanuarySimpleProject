cba61f2cbe5b203c8e35f1305d906980df4290a2
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-01-26 18:30:44 +0300

    сортировка пузырьком

--- JanuarySimpleProject/NewNode/programm.cs
+++ JanuarySimpleProject/NewNode/programm.cs
@@ -22,8 +22,30 @@ namespace JanuarySimpleProject.NewNode
 
             node.RemoveValue("789");
             node.ShowInfo();
+            List<int> numbers = new List<int> { 5, 2, 8, 1, 4 };
+            BubbleSort(numbers);
+            Console.WriteLine(string.Join(" ", numbers));
+
+            Console.ReadLine();
 
             Console.ReadLine();
         }
+        static void BubbleSort(List<int> numbers)
+        {
+            int n = numbers.Count;
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (numbers[j] > numbers[j + 1])
+                    {
+                        int temp = numbers[j];
+                        numbers[j] = numbers[j + 1];
+                        numbers[j + 1] = temp;
+                    }
+                }
+            }
+        }
     }
 }
+  
