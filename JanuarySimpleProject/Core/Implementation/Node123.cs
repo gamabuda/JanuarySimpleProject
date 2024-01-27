355dcdaedf9d2c7bfa1ad1ce591ba396a29874c1
Сарюна Садыкова <sarunasadykova5@gmail.com>
2024-01-27 12:20:08 +0300

    был создан и заполнен Node123.cs на основе Node.cs

--- JanuarySimpleProject/Core/Implementation/Node123.cs
+++ JanuarySimpleProject/Core/Implementation/Node123.cs
@@ -1,38 +1,30 @@
-﻿using System.Text.Json;
-using JanuarySimpleProject.Core.Implementation;
-
-namespace JanuarySimpleProject.Core
+﻿namespace JanuarySimpleProject.Core.Implementation
 {
-    public class Node : INode
+    public class Node1 : INode
     {
-        //TODO switch list to arrayЗАКРЫТО
-        private string[] _values = Array.Empty<string>();
+        private List<string> _values = new List<string>();
         private string _value;
 
-        private Node()
+        public Node1()
         {
             Id = Guid.NewGuid().ToString();
-
             Name = "NewNode";
             DateTimeCreate = DateTime.Now;
             DateTimeUpdate = DateTimeCreate;
 
             OnNodeChange += CheckNode;
             OnNodeChange += DateTimeEditChange;
-            OnNodeChange += SerializeNode2Json;
         }
 
-        public Node(string name)
+        public Node1(string name)
         {
             Id = Guid.NewGuid().ToString();
-
             Name = name;
             DateTimeCreate = DateTime.Now;
             DateTimeUpdate = DateTimeCreate;
 
             OnNodeChange += CheckNode;
             OnNodeChange += DateTimeEditChange;
-            OnNodeChange += SerializeNode2Json;
         }
 
         public string Id { get; }
@@ -44,49 +36,26 @@ namespace JanuarySimpleProject.Core
             get => _value;
             set
             {
-                //ОПТИМИЗАЦИЯ ЗАКРЫТА
                 _value = value?.Trim() ?? string.Empty;
-                _values = new string[] { _value };
+                _values.Clear();
+                _values.Add(_value);
                 OnNodeChange?.Invoke();
             }
         }
-
-        public string JSON { get; set; }
 
         public DateTime DateTimeCreate { get; }
         public DateTime DateTimeUpdate { get; private set; }
 
         public event Action OnNodeChange;
 
-        private void SerializeNode2Json()
-        {
-            JSON = JsonSerializer.Serialize(this);
-        }
-
-        private void DateTimeEditChange()
-        {
-            DateTimeUpdate = DateTime.Now;
-        }
-
-        private void CheckNode()
-        {
-            var temp = String.Empty;
-            foreach (var v in _values)
-                temp += v;
-
-            if (_value != temp)
-                throw new Exception("Node is not correct or broken");
-        }
-
         public void ShowInfo()
         {
             Console.WriteLine($"Node:\tName:{Name} ID:{Id}\n\tDateTime create:{DateTimeCreate}\n\tDateTime last update:{DateTimeUpdate}\n\tValue:{Value}");
         }
 
-        //TODO switch all returns to throw ExceptionЗАКРЫТО
         public void AddValue<TValue>(TValue value)
         {
-            string strValue = value.ToString().Trim();
+            string strValue = value?.ToString().Trim();
 
             if (string.IsNullOrEmpty(strValue))
                 throw new ArgumentException("Value is null or empty.");
@@ -94,36 +63,12 @@ namespace JanuarySimpleProject.Core
             if (_values.Contains(strValue))
                 throw new ArgumentException("Value already exists in the list.");
 
-            _values = new string[] { strValue };
+            _values.Add(strValue);
             _value += strValue;
 
             OnNodeChange?.Invoke();
         }
 
-        //TODO switch all returns to throw Exception ЗАКРЫТО
-        public void AddValue<TValue>(List<TValue> values)
-        {
-            if (values.Count <= 0)
-                throw new ArgumentException("List is empty.");
-
-            foreach (var value in values)
-            {
-                string strValue = value?.ToString().Trim();
-
-                if (string.IsNullOrEmpty(strValue))
-                    throw new ArgumentException("Value is null or empty.");
-
-                if (_values.Contains(strValue))
-                    throw new ArgumentException("Value already exists in the list.");
-
-                _values = new string[] { strValue };
-                _value += strValue;
-
-                OnNodeChange?.Invoke();
-            }
-        }
-
-        //TODO switch all returns to throw Exception and add the ability to delete a list of objects
         public void RemoveValue<TValue>(TValue value)
         {
             string strValue = value?.ToString().Trim();
@@ -134,11 +79,12 @@ namespace JanuarySimpleProject.Core
             if (!_values.Contains(strValue))
                 throw new ArgumentException("Value does not exist in the list.");
 
-            _values = Array.FindAll(_values, v => v != strValue);
+            _values.Remove(strValue);
             _value = Value.Replace(strValue, "");
 
             OnNodeChange?.Invoke();
         }
+
         public void UpdateValue<TValue>(TValue value)
         {
             string strValue = value?.ToString().Trim();
@@ -147,14 +93,25 @@ namespace JanuarySimpleProject.Core
                 throw new ArgumentException("Value is null or empty.");
 
             _value = strValue;
+            _values = new List<string> { _value };
 
             OnNodeChange?.Invoke();
         }
 
-        public static Node CreateEmptyNode()
+        private void DateTimeEditChange()
         {
-            return new Node();
+            DateTimeUpdate = DateTime.Now;
         }
+
+        private void CheckNode()
+        {
+            var temp = string.Empty;
+            foreach (var v in _values)
+                temp += v;
+
+            if (_value != temp)
+                throw new Exception("Node is not correct or broken");
+        }
+
     }
 }
-
