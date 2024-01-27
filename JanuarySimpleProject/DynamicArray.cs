bb32a7930b6d69ad46c909a7cc02171bc8d31098
лиза <ilizagalina4@gmail.com>
2024-01-27 08:43:01 +0300

    бинарный поиск

--- JanuarySimpleProject/DynamicArray.cs
+++ JanuarySimpleProject/DynamicArray.cs
@@ -65,6 +65,29 @@ namespace JanuarySimpleProject
             }
         }
 
+        public int BinarySearch(T value)
+        {
+            Array.Sort(_array);
+            int left = 0;
+            int right = _count - 1;
+
+            while (left <= right)
+            {
+                int mid = left + (right - left) / 2;
+
+                if (_array[mid].CompareTo(value) == 0)
+                    return mid;
+
+                if (_array[mid].CompareTo(value) < 0)
+                    left = mid + 1;
+
+                else
+                    right = mid - 1;
+            }
+
+            return -1;
+        }
+
         public void Print()
         {
             for (int i = 0; i < _count; i++)
