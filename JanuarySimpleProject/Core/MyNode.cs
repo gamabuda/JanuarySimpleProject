aa810a8a7321432c768355fcab15aa315f80b421
unsighera <eddy.weston@bk.ru>
2024-01-28 23:10:58 +0300

    MyNode : INode добавил типо

---
+++ JanuarySimpleProject/Core/MyNode.cs
@@ -0,0 +1,50 @@
+﻿using JanuarySimpleProject.Core.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuarySimpleProject.Core
+{
+    public class MyNode : INode
+    {
+        public MyNode()
+        {
+        }
+
+        public string Id { get; }
+        public string Name { get; set; }
+        public string Value { get; set; }
+
+        public DateTime DateTimeCreate { get; }
+        public DateTime DateTimeUpdate { get; private set; }
+
+        public event Action OnNodeChange;
+
+        public void AddValue<TValue>(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RemoveValue<TValue>(TValue value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void ShowInfo()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UpdateValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TValue UpdateValue<TValue>(TValue oldValue, TValue newValue)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
