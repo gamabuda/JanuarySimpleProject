140f1170ec1b7be2c6a9dadbe9f9757a33425cb6
unsighera <eddy.weston@bk.ru>
2024-01-28 23:08:47 +0300

    IEnumerable начало работы

---
+++ JanuarySimpleProject/Core/MyClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuarySimpleProject.Core
+{
+    public class MyClass<TValue> : IEnumerable, IEquatable<MyClass<TValue>>
+    {
+        public TValue Value { get; set; }
+        private TValue[] Values { get; set; } = new TValue[10];
+
+        public void Add(TValue value) => Values[0] = value;
+
+        public IEnumerator GetEnumerator() => Values.GetEnumerator();
+
+        public bool Equals(MyClass<TValue> other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+
+            return EqualityComparer<TValue>.Default.Equals(Value, other.Value);
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != typeof(MyClass<TValue>)) return false;
+
+            return Equals((MyClass<TValue>)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return EqualityComparer<TValue>.Default.GetHashCode(Value);
+        }
+    }
+}
---
+++ JanuarySimpleProject/Core/TClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JanuarySimpleProject.Core
+{
+    internal class TClass<TValue>
+    {
+        private TValue _value;
+
+        public TClass(TValue value)
+        {
+            _value = value;
+        }
+        public void ShowTType()
+        {
+            Console.WriteLine(_value.GetType());
+        }
+    }
+}
