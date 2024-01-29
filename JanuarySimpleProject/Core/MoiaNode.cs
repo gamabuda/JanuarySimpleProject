cb4cc3f882c8cfe49b33932248800c4d148285c6
212203 <212203@WINMAC-4>
2024-01-29 15:46:33 +0300

    1

--- JanuarySimpleProject/Core/MoiaNode.cs
+++ JanuarySimpleProject/Core/MoiaNode.cs
@@ -1,38 +1,48 @@
-﻿//using JanuarySimpleProject.Core.Implementation;
-//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using JanuarySimpleProject.Core.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace JanuarySimpleProject.Core
-//{
-//    public class MoiaNode : INode
-//    {
-//        public MoiaNode()
-//        {
+namespace JanuarySimpleProject.Core
+{
+    public class MoiaNode : INode
+    {
+        private string _value;
+        private DinamicArray<string> _values = new DinamicArray<string>();
+        string Id { get; }
+        string Name { get; set; }
+        string Value { get; set; }
+        DateTime DateTimeCreate { get; }
+        DateTime DateTimeUpdate { get; }
+        public event Action OnNodeChange;
 
-//        }
-//        string Id { get; }
-//        string Name { get; set; }
-//        string Value { get; set; }
-//        DateTime DateTimeCreate { get; }
-//        DateTime DateTimeUpdate { get; }
+        void ShowInfo()
+        {
+            Console.WriteLine($"Node:\tName:{Name} ID:{Id}\n\tDateTime create:{DateTimeCreate}\n\tDateTime last update:{DateTimeUpdate}\n\tValue:{Value}");
+        }
+        void AddValue<TValue>(TValue value)
+        {
+            string strValue = value.ToString().Trim();
+            _values.Add(strValue);
+            _value += $"{strValue}";
+            OnNodeChange?.Invoke();
+        }
+        void RemoveValue<TValue>(TValue value)
+        {
+            foreach (TValue value in values)
+            {
+                string strValue = value.ToString().Trim();
 
-//        void ShowInfo()
-//        {
+                _values.Remove(strValue);
+                _value = Value.Replace(strValue, "");
 
-//        }
-//        void AddValue<TValue>(TValue value)
-//        {
+                OnNodeChange?.Invoke();
+            }
+        }
+        
 
-//        }
-//        void RemoveValue<TValue>(TValue value)
-//        {
-
-//        }
-      
-
-//        event Action OnNodeChange;
-//    }
-//}
+       
+    }
+}
