a1f7747705a93f2053bf563e3cb6f718731627f8
Сарюна Садыкова <sarunasadykova5@gmail.com>
2024-01-29 17:39:22 +0300

    сортировка и бинарный поиск

--- JanuarySimpleProject/Core/Implementation/DArray.cs
+++ JanuarySimpleProject/Core/Implementation/DArray.cs
@@ -1,4 +1,6 @@
-﻿public class DynamicArray<T>
+﻿using System;
+
+public class DynamicArray<T> where T : IComparable<T>
 {
     private T[] array;
     private int count;
@@ -53,5 +55,49 @@
             Console.Write(array[i] + " ");
         }
         Console.WriteLine();
+    }
+
+    public void Sort()
+    {
+        for (int i = 0; i < count - 1; i++)
+        {
+            for (int j = 0; j < count - i - 1; j++)
+            {
+                if (array[j].CompareTo(array[j + 1]) > 0)
+                {
+                    T temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+    }
+
+    public int BinarySearch(T item)
+    {
+        int left = 0;
+        int right = count - 1;
+
+        while (left <= right)
+        {
+            int mid = left + (right - left) / 2;
+
+            int comparisonResult = item.CompareTo(array[mid]);
+
+            if (comparisonResult == 0)
+            {
+                return mid;
+            }
+            else if (comparisonResult < 0)
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
     }
 }
