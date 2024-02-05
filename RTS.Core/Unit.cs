8b9ef40ec1c4e2164ae976778d4fb8dee5556947
212216 <212216@WINMAC12>
2024-02-05 16:29:37 +0300

    начало работы с RTS

---
+++ RTS.Core/Unit.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS.Core
+{
+    public class Unit
+    {
+        public int Health { get; set; }
+        public int Mana { get; set; }
+
+        public int Strength { get; set; }
+        public int Dexterity { get; set; }
+        public int Intelligence { get; set; }
+        public int Virality { get; set; }
+        public int Damage { get; set; }
+
+        public Unit(int strength, int dexterity, int intelligence, int virality)
+        {
+            this.Strength = strength;
+            this.Dexterity = dexterity;
+            this.Intelligence = intelligence;
+            this.Virality = virality;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Health: {Health}\n" +
+                $"Mana: {Mana}\n" +
+                $"Strength: {Strength}\n" +
+                $"Dexterity: {Dexterity}\n" +
+                $"Intelligence: {Intelligence}\n" +
+                $"Virality: {Virality}");
+        }
+
+        public void Attack(Unit unit, int damage)
+        {
+            unit.Health -= damage;
+        }
+    }
+}
---
+++ RTS.Core/Wizard.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS.Core
+{
+    internal class Wizard : Unit
+    {
+        public Wizard(int strength, int dexterity, int intelligence, int virality) : base(strength, dexterity, intelligence, virality)
+        {
+            Health = (int)(1.4 / virality + 0.2 / Strength);
+        }
+    }
+}
