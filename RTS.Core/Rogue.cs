eb45f1671a2f70161879b37aa4ab3fb7ce065ff8
212201 <212201@1220-012-22>
2024-02-07 10:14:21 +0300

    Созданы 3 дочерних класса

--- RTS.Core/Rogue.cs
+++ RTS.Core/Rogue.cs
@@ -6,7 +6,13 @@ using System.Threading.Tasks;
 
 namespace RTS.Core
 {
-    internal class Rogue
+    internal class Rogue : Unit
     {
+        public Rogue(int Strenght, int Dexterity, int Inteligense, int Vitality) : base(Strenght, Dexterity, Inteligense, Vitality)
+        {
+            double Health = 1.5 / Vitality + 0 / 5 / Strenght;
+            double Mana = 1.2 / Inteligense;
+        }
+
     }
 }
--- RTS.Core/Unit.cs
+++ RTS.Core/Unit.cs
@@ -1,7 +1,32 @@
 ﻿namespace RTS.Core
 {
-    public class Class1
+    public class Unit
     {
+        public int Vitality { get; set; }
+        public int Mana { get; set; }
+        public int Health { get; set; }
+        public int Strenght { get; set; }
+        public int Dexterity { get; set; }
+        public int Inteligense { get; set; }
 
+        public Unit(int Strenght, int Dexterity, int Inteligense, int Vitality)
+        {
+            this.Strenght = Strenght;
+            this.Dexterity = Dexterity;
+            this.Inteligense = Inteligense;
+            this.Vitality = Vitality;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Health: {Health}\nMana: {Mana}\nS: {Strenght}\nD: {Dexterity}\nI: {Inteligense}\nV: {Vitality}");
+        }
+        public void Damage()
+        {
+
+        }
+
+        public void Attack()
+        {
+
+        }
     }
-}
