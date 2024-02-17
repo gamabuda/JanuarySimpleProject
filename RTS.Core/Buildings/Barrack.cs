3ab9756ea6516be03f6d14f375f7d5cf01aaf6f8
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-02-17 16:30:07 +0300

    откат к версии 6

---
+++ RTS.Core/Buildings/Barrack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS.Core.Buildings
+{
+    public class Barrack
+    {
+        public int hp { get; set; }
+
+        public static Unit CreateUnit(string type)
+        {
+            switch (type.ToLower())
+            {
+                case "warrior":
+                    return new Warrior();
+                    break;
+                case "rogue":
+                    return new Rogue();
+                    break;
+                case "wizard":
+                    return new Wizard();
+                    break;
+                default:
+                    throw new Exception("This unit doesn`t exist");
+            }
+
+            return null;
+        }
+    }
+}
