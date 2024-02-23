e030804cbdcd9beb6f9b749bc7a08188a82e2d41
Samat <samatsafiullin7@gmail.com>
2024-02-23 11:01:41 +0300

    thinking

--- RTS.WPF2/MainWindow.xaml.cs
+++ RTS.WPF2/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,22 +13,108 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using RTS.Core;
 
 namespace RTS.WPF2
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
+    
     public partial class MainWindow : Window
     {
+        private Unit _unit;
+        private Barracks _barracks;
         public MainWindow()
         {
             InitializeComponent();
+            Barracks barracks = new Barracks();
+            _unit = new Unit();
+            DataContext = _unit;
         }
 
+        public class Unit : INotifyPropertyChanged
+        {
+            Warrior warrior =new Warrior();
+            private Unit _unit;
+            private Rogue _rogue;
+            private Warrior _warrior;
+            private Wizzard _wizzard;
+            private double _strength;
+            private double _vitality;
+            private double _dexterity;
+            private double _inteligence;
+
+            public Unit unit { get { return _unit; } set { _unit = value; } }
+            public Rogue Rogue { get { return _rogue; } set { _rogue = value; } }
+            public Warrior Warrior { get { return _warrior; } set { _warrior = value; } }
+            public Wizzard  Wizzard { get { return _wizzard; } set { _wizzard = value; } }
+            public double Strength
+            {
+                get { return _strength; }
+                set
+                {
+                    if (_strength != value)
+                    {
+                        _strength = value;
+                        OnPropertyChanged("Strength");
+                    }
+                }
+            }
+
+            public double Vitality
+            {
+                get { return _vitality; }
+                set
+                {
+                    if (_vitality != value)
+                    {
+                        _vitality = value;
+                        OnPropertyChanged("Vitality");
+                    }
+                }
+            }
+
+            public double Inteligence
+            {
+                get { return _vitality; }
+                set
+                {
+                    if (_inteligence != value)
+                    {
+                        _inteligence = value;
+                        OnPropertyChanged("Dexterity");
+                    }
+                }
+            }
+
+            public double Dexterity
+            {
+                get { return _vitality; }
+                set
+                {
+                    if (_dexterity != value)
+                    {
+                        _dexterity = value;
+                        OnPropertyChanged("Dexterity");
+                    }
+                }
+            }
+
+            public event PropertyChangedEventHandler PropertyChanged;
+
+            protected virtual void OnPropertyChanged(string propertyName)
+            {
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+            }
+
+           
+        }
         private void SBTNNplus_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Ahead_Click(object sender, RoutedEventArgs e)
+        {
+              
         }
     }
 }
