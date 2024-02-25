0eec84ef7ac91993cce31284969c83201c1196a8
Ильнара <kilkahuzina@gmail.com>
2024-02-25 18:30:35 +0300

    интерфейсы до конца

---
+++ rts.core/IAttackHandler8q8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS.Core
+{
+    public interface IAttackHandler
+    {
+        public int Damage { get; set; }
+        public int MagicalDamage { get; set; }
+        public int CriticalDamage { get; set; }
+        public void Attack(Unit unit) { }
+    }
+}
