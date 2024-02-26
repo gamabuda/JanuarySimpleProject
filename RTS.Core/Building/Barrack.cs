c8b200d33d0c6b57b1aeebecf745bcb66c83301c
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-02-26 08:49:56 +0300

    add building

---
+++ RTS.Core/Building/Barrack.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RTS.Core.Characters;
+using System.Threading;
+
+namespace RTS.Core.Building
+{
+    public class Barrack
+    {
+        public Unit CreateCharacter(string characterType)
+        {
+            switch (characterType.ToLower())
+            {
+                case "warrior":
+                    return new Warrior();
+                case "rogue":
+                    return new Rogue();
+                case "wizard":
+                    return new Wizard();
+                default:
+                    Console.WriteLine("Wrong character type. Warrior is selected by default.");
+                    return new Warrior();
+            }
+        }
+    }
+}
---
+++ RTS.Core/Building/Church.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using RTS.Core.Characters;
+
+namespace RTS.Core.Building
+{
+    public class Church
+    {
+        public void Pray(Unit unit)
+        {
+            if (unit is Wizard)
+            {
+                Wizard wizard = (Wizard)unit;
+                wizard.Mana *= 2;
+                Console.WriteLine($"Wizard prayed in the Church. Mana: {wizard.Mana}");
+            }
+            else if (unit is Rogue)
+            {
+                Rogue rogue = (Rogue)unit;
+                rogue.Mana /= 2;
+                Console.WriteLine($"Rogue prayed in the Church. Mana: {rogue.Mana}");
+            }
+            else if (unit is Warrior)
+            {
+                Warrior warrior = (Warrior)unit;
+                warrior.Mana *= 1;
+                Console.WriteLine($"Warrior prayed in the Church. Mana: {warrior.Mana}");
+            }
+        }
+    }
+}
