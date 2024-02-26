15d14c2a7dcdce017cfc1c550939031533bdb2ae
user <user@DESKTOP-RA6T6M6>
2024-02-26 22:10:49 +0300

    впф не готово

--- RTS.WPF/MainWindow.cs
+++ RTS.WPF/MainWindow.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using RTS.Core;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,14 +9,43 @@ using System.Windows;
 
 namespace RTS.WPF
 {
-    public partial class MainWindow : Window
-    {
-        private void strPlus_Click(object sender, RoutedEventArgs e)
-        {
-            if (Strenght1 == Strenght3)
-                return;
-            else
-                Strenght1 += 1;
-        }
-    }
+    //public partial class MainWindow : Window
+    //{
+    //    private Unit _unit;
+    //    public MainWindow()
+    //    {
+    //        InitializeComponent();
+    //        _unit = new Unit();
+    //        DataContext = _unit;
+    //    }
+    //    private void strPlus_Click(object sender, RoutedEventArgs e)
+    //    {
+    //        if (Strenght1 == Strenght3)
+    //            return;
+    //        else
+    //            unit.Strength1 += 1;
+    //    }
+    //}
+
+    //public class Warrior
+    //{
+    //    public string Strenght1
+    //    {
+    //        get { return Strenght1; }
+    //        set
+    //        {
+    //            if (Strenght1 != value)
+    //            {
+    //                Strenght1 = value;
+    //                OnPropertyChanged("Strenght1");
+    //            }
+    //        }
+    //    }
+
+    //    public event PropertyChangedEventHandler PropertyChanged;
+    //    protected virtual void OnPropertyChanged(string propertyName)
+    //    {
+    //        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    //    }
+    //}
 }
