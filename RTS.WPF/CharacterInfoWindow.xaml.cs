4ee845fc1d4fffc05707c27852f40ba6b4a52fca
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-02-26 09:06:10 +0300

    add wpf

---
+++ RTS.WPF/CharacterInfoWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using RTS.Core.Characters;
+
+namespace RTS.WPF
+{
+    public partial class CharacterInfoWindow : Window
+    {
+        private Unit unit;
+
+        public CharacterInfoWindow(Unit unit)
+        {
+            InitializeComponent();
+            this.unit = unit;
+            DisplayCharacterInfo();
+        }
+
+        private void DisplayCharacterInfo()
+        {
+            DataContext = unit;
+        }
+
+        private void Strength_Click(object sender, RoutedEventArgs e)
+        {
+            unit.Strength++;
+        }
+
+        private void Dexterity_Click(object sender, RoutedEventArgs e)
+        {
+            unit.Dexterity++;
+        }
+
+        private void Intelligence_Click(object sender, RoutedEventArgs e)
+        {
+            unit.Intelligence++;
+        }
+
+        private void Vitality_Click(object sender, RoutedEventArgs e)
+        {
+            unit.Vitality++;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            unit.GetExperience(100);
+        }
+    }
+}
---
+++ RTS.WPF/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using RTS.Core.Characters;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RTS.WPF
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void WarriorButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenCharacterInfoWindow(new Warrior());
+        }
+
+        private void RogueButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenCharacterInfoWindow(new Rogue());
+        }
+
+        private void WizardButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenCharacterInfoWindow(new Wizard());
+        }
+
+        private void OpenCharacterInfoWindow(Unit unit)
+        {
+            CharacterInfoWindow characterInfoWindow = new CharacterInfoWindow(unit);
+            characterInfoWindow.Show();
+        }
+    }
+}
