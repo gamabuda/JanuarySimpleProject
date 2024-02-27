72c4e82037d1652d512a79dca0b6b29971da0628
Камила Фаттахова <kamilafattahova65210@gmail.com>
2024-02-28 01:19:16 +0300

    add peasant and catapult

---
+++ RTS.Core/Building/Catapult.cs
@@ -0,0 +1,38 @@
+﻿using RTS.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS.Core.Building
+{
+    public class Catapult : IHealthHandler, IAttackHandler
+    {
+        public int MaxHealth { get; set; }
+        public int Health { get; set; }
+        public int Armor { get; set; }
+        public int PDamage { get; set; }
+        public int MDamage { get; set; }
+        public int CrtDamage { get; set; }
+
+        public Catapult()
+        {
+            MaxHealth = Health;
+            Health = 150;
+            Armor = 20;
+            PDamage = 50;
+            MDamage = 0;
+            CrtDamage = 100;
+        }
+
+        public void Attack(IHealthHandler target)
+        {
+            int damageDealt = PDamage - target.Armor;
+            if (damageDealt < 0)
+                damageDealt = 0;
+
+            target.Health -= damageDealt;
+        }
+    }
+}
---
+++ RTS.Core/Characters/Peasant.cs
@@ -0,0 +1,30 @@
+﻿using RTS.Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RTS.Core.Characters
+{
+    public class Peasant : Unit
+    {
+        public Peasant()
+        {
+            Image = ".\\RTS.WPF\\images\\peasant.jpg";
+            Name = "Peasant";
+            Strength = 1;
+            Dexterity = 1;
+            Intelligence = 1;
+            Vitality = 1;
+            MaxStrength = 1;
+            MaxDexterity = 1;
+            MaxIntelegence = 1;
+            MaxVitality = 1;
+
+            Health = 100;
+
+            MaxHealth = Health;
+        }
+    }
+}
