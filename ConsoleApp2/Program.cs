29c33a0db0ec2a04b0e25d58b1a07334a61736c2
MorFyNC <bulat.hisambeev@gmail.com>
2024-02-28 01:10:52 +0300

    Удалил лишние файлы

--- ConsoleApp2/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using Classes;
-
-Warrior warrior = new Warrior();
-Wizard wizard = new Wizard();
-wizard.Fireball(warrior);
-Console.WriteLine();
--- ConsoleApp3/Program.cs
+++
@@ -1,5 +0,0 @@
-﻿using Classes;
-
-IHealthHandler healthHandler = new Warrior();
-var armorgetter = typeof(IArmorHandler).GetMethod("get_Armor");
-int Armor = Convert.ToInt32(armorgetter?.Invoke(healthHandler, null));
